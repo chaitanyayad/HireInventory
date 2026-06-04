43d4f350dae5b73701553e29a3496ac99838ef71
Chaitanya Yadav <chaitanyayadav10e2597@gmail.com>
2026-06-04 14:26:32 +0530

    installed pydantic-settings to read credentials from env files

--- job-tracker/backend/app/config.py
+++ job-tracker/backend/app/config.py
@@ -1,1 +1,11 @@
-# for reading credentials from env file for postgres 
+# for reading credentials from env file for postgres ,redis, api keys anything
+
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
